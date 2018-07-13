03d5006c51f353c250b7dc3dd584a0312c57de85
onetoughcookie226 <onetoughcookie226@gmail.com>
2018-07-13 15:31:54 +0200

    Set up json server, send an ajax request with jQuery

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-$("#my-button").click(function(){
-    $.ajax({url: "http://localhost:8080/jobs/1", success: function(result){
-        $("#my-div").html(result);
-        // console.log(result);
-    }});
-});
-
-
-// $(document).ready(function(){
-//     $("button").click(function(){
-//         $("#div1").load("demo_test.txt");
-//     });
+// $("#my-button").click(function(){
+//     $.ajax({url: "http://localhost:8080/jobs/1", success: function(result){
+//         $("#my-div").html(result);
+//         // console.log(result);
+//     }});
 // });
+
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#my-div").load("http://localhost:8080/jobs/1");
+    });
+});
+// $("#result").load("http://localhost:8080/jobs/1");
+
